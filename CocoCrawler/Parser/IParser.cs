59357acee1f7d7ead77c66dc515065604a95f336
David Robinson <dr@consit.dk>
2024-09-26 10:17:50 +0200

    Add the possibility to transform the URL in "OpenLink".

--- CocoCrawler/Parser/IParser.cs
+++ CocoCrawler/Parser/IParser.cs
@@ -1,4 +1,5 @@
-﻿using CocoCrawler.Job.PageTasks;
+﻿using AngleSharp.Dom;
+using CocoCrawler.Job.PageTasks;
 using Newtonsoft.Json.Linq;
 
 namespace CocoCrawler.Parser;
@@ -6,7 +7,7 @@ namespace CocoCrawler.Parser;
 public interface IParser
 {
     Task Init(string html);
-    string[] ParseForLinks(string linksSelector);
+    string[] ParseForLinks(string linksSelector, Func<IElement, string?>? linkProcessor = null);
     JArray ExtractList(CrawlPageExtractListTask scrapeList);
     JObject ExtractObject(CrawlPageExtractObjectTask task);
 }
